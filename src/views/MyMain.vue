24e686b39ac0edbcf998b42a8ff9da7e932bd108
Muhammad Fadll <fadeljourney@gmail.com>
2023-01-28 20:59:31 +0700

    change values object

--- src/views/MyMain.vue
+++ src/views/MyMain.vue
@@ -17,30 +17,25 @@
           <div v-for="game in gamesData" :key="game" class="card">
             <div
               class="card-img"
-              :style="`background-image: url(${game.image})`"
+              :style="`background-image: url(${require(`@/assets/${game.image}`)})`"
             >
               <h2><img :src="game.logo" :alt="game.name" /></h2>
             </div>
           </div>
         </div>
+        <button class="">More games</button>
       </div>
-      <button class="">More games</button>
     </article>
   </main>
 </template>
 <script>
+// files SVG
 import nfsHeatLogo from "@/assets/nfs-heat.svg";
-import nfsHeatImage from "@/assets/nfs-heat.jpg";
 import nfsPursuitLogo from "@/assets/nfs-hot-pursuit.svg";
-import nfsPursuitImage from "@/assets/nfs-hot-pursuit.jpg";
 import nfsPaybackLogo from "@/assets/nfs-payback.svg";
-import nfsPaybackImage from "@/assets/nfs-payback.jpg";
 import nfsRivalsLogo from "@/assets/nfs-rivals.svg";
-import nfsRivalsImage from "@/assets/nfs-rivals.jpg";
 import nfsNoLimitsLogo from "@/assets/nfs-no-limits.svg";
-import nfsNoLimitsImage from "@/assets/nfs-no-limits.jpg";
 import nfsMobileLogo from "@/assets/nfs-mobile.svg";
-import nfsMobileImage from "@/assets/nfs-mobile.jpg";
 
 export default {
   name: "MyMain",
@@ -53,32 +48,32 @@ export default {
         {
           name: "NFS heat",
           logo: nfsHeatLogo,
-          image: nfsHeatImage,
+          image: "nfs-heat.jpg",
         },
         {
           name: "NFS hot pursuit",
           logo: nfsPursuitLogo,
-          image: nfsPursuitImage,
+          image: "nfs-hot-pursuit.jpg",
         },
         {
           name: "NFS payback",
           logo: nfsPaybackLogo,
-          image: nfsPaybackImage,
+          image: "nfs-payback.jpg",
         },
         {
           name: "NFS rivals",
           logo: nfsRivalsLogo,
-          image: nfsRivalsImage,
+          image: "nfs-rivals.jpg",
         },
         {
           name: "NFS no limits",
           logo: nfsNoLimitsLogo,
-          image: nfsNoLimitsImage,
+          image: "nfs-no-limits.jpg",
         },
         {
           name: "NFS mobile",
           logo: nfsMobileLogo,
-          image: nfsMobileImage,
+          image: "nfs-mobile.jpg",
         },
       ],
     };
